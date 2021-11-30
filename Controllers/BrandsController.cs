665d26ce6f63758282a79f7e50dc5c540a8dd8f9
vinu01 <vinodkumar15190@gmail.com>
2021-11-30 23:06:50 +0530

    Update categories,brand,products pagee

---
+++ Controllers/BrandsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DemoWebApp.Models;
+
+namespace DemoWebApp.Controllers
+{
+    public class BrandsController : Controller
+    {
+        // GET: Brands
+        public ActionResult Index()
+        {
+            EFDBFirstDatabaseEntities db = new EFDBFirstDatabaseEntities();
+            List<Brand> brands = db.Brands.ToList();
+            return View(brands);
+        }
+    }
+}
---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DemoWebApp.Models;
+
+namespace DemoWebApp.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            EFDBFirstDatabaseEntities db = new EFDBFirstDatabaseEntities();
+            List<Product> products = db.Products.ToList();
+            return View(products);
+        }
+    }
+}
